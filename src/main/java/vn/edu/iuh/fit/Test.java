0edae9b51eaedfd7c116268df0f898827a2865b0
hhiep <hoaihiep12b1thptlochung2020@gmail.com>
2023-09-17 16:18:52 +0700

    tạo project, tạo thư mục models và lớp Employee và 1 lớp để kiểm tra mapping sang mariadb

---
+++ src/main/java/vn/edu/iuh/fit/Test.java
@@ -0,0 +1,21 @@
+package vn.edu.iuh.fit;
+
+import jakarta.persistence.*;
+
+public class Test {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-persistence");
+        EntityManager entityManager = emf.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        try {
+            transaction.begin();
+
+            transaction.commit();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            transaction.rollback();
+        }
+        entityManager.close();
+    }
+}
---
+++ src/main/java/vn/edu/iuh/fit/models/Employee.java
@@ -0,0 +1,132 @@
+package vn.edu.iuh.fit.models;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "Employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "address", length = 250, nullable = false)
+    private String address;
+    @Column(name = "dob", nullable = false)
+    private LocalDateTime dob;
+    @Column(name = "email", length = 150)
+    private String email;
+    @Column(name = "full_name", length = 150, nullable = false)
+    private String fullName;
+    @Column(name = "phone", length = 15, nullable = false)
+    private String phone;
+    @Column(name = "status",  nullable = false)
+    @Enumerated(EnumType.ORDINAL)
+    private EmployeeStatus status;
+
+    public Employee() {
+    }
+
+    public Employee(String address, LocalDateTime dob, String email, String fullName, String phone, EmployeeStatus status) {
+        this.address = address;
+        this.dob = dob;
+        this.email = email;
+        this.fullName = fullName;
+        this.phone = phone;
+        this.status = status;
+    }
+
+    public Employee(long id, String address, LocalDateTime dob, String email, String fullName, String phone, EmployeeStatus status) {
+        this.id = id;
+        this.address = address;
+        this.dob = dob;
+        this.email = email;
+        this.fullName = fullName;
+        this.phone = phone;
+        this.status = status;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public LocalDateTime getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDateTime dob) {
+        this.dob = dob;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public EmployeeStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(EmployeeStatus status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", address='" + address + '\'' +
+                ", dob=" + dob +
+                ", email='" + email + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", phone='" + phone + '\'' +
+                ", status=" + status +
+                '}';
+    }
+}
